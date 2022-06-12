7f664aa6e2e660c6b361ced38d02789b666dbbfa
Aashrey Sharma <aashrey9sharma@gmail.com>
2022-06-12 17:47:42 -0400

    Updated icon fills

--- src/icons/icon_copy.tsx
+++ src/icons/icon_copy.tsx
@@ -3,8 +3,8 @@ import { h } from "preact"
 export const CopyIcon = function(props: any) {
   return (
     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3.5" y="3.5" width="8" height="8" stroke="black"/>
-    <path d="M9 0.5H0.5V9" stroke="black"/>
+    <rect x="3.5" y="3.5" width="8" height="8" stroke="#000000"/>
+    <path d="M9 0.5H0.5V9" stroke="#000000"/>
     </svg>
   )
 }
--- src/icons/icon_minus.tsx
+++ src/icons/icon_minus.tsx
@@ -2,6 +2,6 @@ import { h } from "preact"
 
 export const MinusIcon = function(props: any) {
   return (
-    <svg class="svg" width="12" height="6" viewBox="0 0 12 6" xmlns="http://www.w3.org/2000/svg"><path d="M11 3H0V2h11v1z" fill-rule="evenodd" fill-opacity="1" fill="#000" stroke="none"></path></svg>
+    <svg class="svg" width="12" height="6" viewBox="0 0 12 6" xmlns="http://www.w3.org/2000/svg"><path d="M11 3H0V2h11v1z" fill-rule="evenodd" fill-opacity="1" fill="#000000" stroke="none"></path></svg>
   )
 }
